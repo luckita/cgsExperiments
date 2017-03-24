c958b2ff4bda068b0b77a70a0cccf4af0a7a030b
Luckita <lucreciagonzalez@gmail.com>
2017-03-24 14:13:36 -0400

    Adding the Unity experiments to github, yay!

---
+++ Assets/cubeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//(GetComponent.<Renderer>().material.mainTexture as MovieTexture).Play();
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
